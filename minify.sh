15cf2e5b06c33095f4006330f96799d4e51b3674
Nathan Laha <nlaha@outlook.com>
2021-09-16 01:19:34 -0700

    fix and add images

--- minify.sh
+++ minify.sh
@@ -1,5 +1,13 @@
+# minify photography images
 for FILE in input_photography/*; do 
     echo "minifying $FILE...";
     filename=$(basename "$FILE")
     magick $FILE -resize 40%x40% -strip -interlace Plane -gaussian-blur 0.05 -quality 85% public/images/photography/$filename; 
+    done
+
+# minify void world images
+for FILE in input_VW/*; do 
+    echo "minifying $FILE...";
+    filename=$(basename "$FILE")
+    magick $FILE -resize 1300x900\> -strip -interlace Plane -gaussian-blur 0.05 -quality 85% public/images/photography/$filename; 
     done
