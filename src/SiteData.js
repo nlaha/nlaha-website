9ade6c35b84519e79264fbe396af5ba9f9b26382
Nathan Laha <nlaha@outlook.com>
2024-02-11 10:45:28 -0800

    Update SiteData.js

--- src/SiteData.js
+++ src/SiteData.js
@@ -60,71 +60,61 @@ var siteData = {
     {
       title: "Premera Blue Cross - Systems Engineering Intern",
       date: "Summer 2023",
-      description: "Full-time summer internship in systems engineering at Premera Blue Cross.",
+      description: "Working as a full-time intern in systems engineering at Premera Blue Cross. I helped the cloud platform team implement policy and Terraform code across an Azure tenant. Ultimately, this enabled 5 teams across the company better utilize Azure while staying compliant with HIPAA and HITRUST standards.",
       url: "https://www.premera.com",
     },
     {
-      title: "Teaching Assistant - WSU VCEA",
+      title: "WSU VCEA - Teaching Assistant",
       date: "Fall Semester 2023",
-      description: "Working as a TA for the CptS 121 class. I teach a 3-hour lab section once a week."
+      description: "Working as a TA for the Voiland college of engineering, I taught a 3-hour lab section once a week, helping 17 students review content and prepare for their exams. I also held office hours where I provided one-on-one assistance to those that needed it."
     },
     {
-      title: "UW Medicine - Temporary Work",
+      title: "UW Medicine - Program Assistant",
       date: "2020 - Present",
-      description:
-        "Working in a temporary position expanding on my previous volunteer position with Laboratory Medicine Informatics. I work on data science and web development projects using Python, R and SQL.",
-      url: "",
+      description: "Working as a part-time software engineer helping to create web apps and data visualizations using Python, R, and SQL. During this time, I developed a full-stack web application to display and analyze laboratory test metrics.",
+      url: "https://www.uwmedicine.org/",
     },
     {
-      title: "UW Medicine - Volunteer Work",
+      title: "UW Medicine - Volunteer",
       date: "2019 - 2020",
-      description:
-        "Assisted Laboratory Medicine Informatics with a variety of small Python and R tasks over the course of a year.",
+      description: "Working as a volunteer helping with small data science tasks in R and Python. During this time, I learned the R programming language and developed an application to replace a SQL script that was no longer maintained. By doing this, I unified a codebase that had previously relied on SQL to manage data intake.",
       url: "",
     },
     {
       title: "Washington State University - B.Sc. CS",
       date: "2021 - Present",
-      description:
-        "Working towards a Bachelor of Science in computer science from the WSU Voiland College of Engineering and Architecture.",
-      url:
-        "https://school.eecs.wsu.edu/academics/undergraduate-program/computer-science/",
+      description: "Working towards a Bachelor of Science in computer science from the WSU Voiland College of Engineering and Architecture.",
+      URL: "https://school.eecs.wsu.edu/academics/undergraduate-program/computer-science/",
     },
     {
       title: "Crimson Code Hackathon",
       date: "2022 - 2022",
-      description:
-        'Participated in the 2022 Crimson Code hackathon with a team of 4. Our web app "Coug Study Network" won first place.',
-      url:
-        "https://dailyevergreen.com/128124/news/students-compete-learn-new-coding-in-annual-hackathon/",
+      description: 'Participated in the 2022 Crimson Code hackathon with a team of 4. Our web app "Coug Study Network" won first place.',
+      URL: "https://dailyevergreen.com/128124/news/students-compete-learn-new-coding-in-annual-hackathon/",
     },
     {
       title: "Ballard High School - APCS",
       date: "2017 - 2021",
-      description:
-        "My first formal computer science class. Used the JAVA programming language and passed the AP exam with a 5/5.",
+      description: "My first formal computer science class. Used the JAVA programming language and passed the AP exam with a 5/5.",
       url: "",
     },
     {
       title: "BHS General Computing Club",
       date: "2019 - 2021",
-      description:
-        "Leader of the BHS General Computing Club (GCC). At GCC we create a space where people of all skill levels can learn about software/game development, data science and graphic art.",
+      description: "Leader of the BHS General Computing Club (GCC). At GCC we create a space where people of all skill levels can learn about software/game development, data science and graphic art.",
       url: "https://gcc.nlaha.com",
     },
   ],
   programming: [
     {
       title: "Coug Study Network",
-      description:
-        "Coug Study Network is a platform for sharing assignment information and study habits. It allows students to make posts about assignments and rate how difficult or time consuming they were. This gives both students and professors valuable information about the classes.",
+      description: "Coug Study Network is a platform for sharing assignment information and study habits. It allows students to make posts about assignments and rate how difficult or time consuming they were. This gives both students and professors valuable information about the classes.",
       url: "https://wsusn.nlaha.com",
       photos: [],
     },
     {
       title: "SPS MC",
-      description:
-        "SPS MC is an Office 365 secured Minecraft server for Seattle Public Schools students. It uses the Office365 API to verify if a player is a part of Seattle Public Schools.",
+      description: "SPS MC is an Office 365 secured Minecraft server for Seattle Public Schools students. It uses the Office365 API to verify if a player is a part of Seattle Public Schools.",
       url: "https://github.com/GCC-Minecraft-Team",
       photos: [],
     },
@@ -135,15 +125,13 @@ var siteData = {
     },
     {
       title: "Godot MIDI",
-      description:
-        "A custom extension for the Godot game engine allowing for MIDI file import. I wrote the MIDI parser from scratch using the MIDI file format specification",
+      description: "Wrote a custom MIDI file parser from scratch both in C# and later in C++ as a plugin for the Godot game engine. This allows developers to incorporate music synchronization into their games or applications easily. It is an open-source project with over thirty stars on GitHub.",
       url: "https://github.com/nlaha/godot-midi-4.0",
       photos: [],
     },
     {
       title: "Japanese Art Swords",
-      description:
-        "A fully custom website I made using CraftCMS, I wrote a python script to scrape and import data from the old drupal site.",
+      description: "A fully custom website I made using CraftCMS, I wrote a python script to scrape and import data from the old Drupal site.",
       url: "https://japaneseartswords.com",
       photos: [],
     },
@@ -164,7 +152,7 @@ var siteData = {
     {
       title: "The Top",
       description:
-        "Composed an original soundtrack for a game, and provided assistance with programming some more advanced features. It is available for purchase on Steam.",
+        "Composed an original soundtrack for a game, and provided additional assistance with programming some more advanced features. It is available for purchase on Steam.",
       url: "https://store.steampowered.com/app/2096560/The_Top/",
     },
     {
@@ -184,7 +172,7 @@ var siteData = {
     {
       title: "Digital Filmmaking",
       description:
-        'I graduated the digital filmmaking program at BHS with a department scholar award. In this program I learned how to create high quality productions and work in a team effectively, even in stressful situations. My animation "Time Tower" won awards at the Northwest High School Film Festival and the National Academy of Television Arts and Sciences - Northwest Emmy Awards.',
+        'I graduated from the digital filmmaking program at BHS with a department scholar award. In this program, I learned how to create high-quality productions and work in a team effectively, even in stressful situations. My animation "Time Tower" won awards at the Northwest High School Film Festival and the National Academy of Television Arts and Sciences - Northwest Student Emmy Awards.',
       url: "https://vimeo.com/559140574",
     },
   ],
