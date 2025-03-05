0b0eebaece65af0fae907671e665b36f4cfa593c
Nathan <nlaha@outlook.com>
2025-03-04 17:14:45 -0800

    2025 Website revamp

--- src/App.vue
+++ src/App.vue
@@ -10,62 +10,43 @@
       <div class="header-div">
         <div class="title-container">
           <h1 class="title site-title">Nathan Laha</h1>
-          <p class="subtitle site-subtitle">Computer Science Student @ WSU</p>
-        </div>
-      </div>
-    </nav>
-    <nav class="panel">
-      <div class="social-container">
-        <div class="social-group panel card">
-          <a
-            role="button"
-            aria-label="Washington State University VCEA"
-            href="https://vcea.wsu.edu"
-            class="social-icon"
-          >
-            <img class="wsu-icon" src="wsu.svg" title="WSU VCEA" />
-          </a>
-          <a
-            role="button"
-            aria-label="Instagram"
-            href="https://instagram.com/nathan.laha"
-            class="social-icon"
-          >
-            <i class="bi bi-instagram" title="Instagram"></i>
-          </a>
-          <a role="button" href="https://github.com/nlaha" class="social-icon">
-            <i class="bi bi-github" title="GitHub"></i>
-          </a>
-          <a
-            role="button"
-            aria-label="Facebook"
-            href="https://www.facebook.com/nathan.laha.3"
-            class="social-icon"
-          >
-            <i class="bi bi-facebook" title="Facebook"></i>
-          </a>
-          <a
-            role="button"
-            aria-label="LinkedIn"
-            href="https://www.linkedin.com/in/nlaha"
-            class="social-icon"
-          >
-            <i class="bi bi-linkedin" title="LinkedIn"></i>
-          </a>
-          <a role="button" href="mailto:nlaha@nlaha.com" class="social-icon">
-            <i class="bi bi-envelope-at-fill" title="Email"></i>
-          </a>
-          <a
-            role="button"
-            aria-label="Download Resume"
-            href="https://github.com/nlaha/nlaha-website-2021/raw/main/NathanLaha_Resume_NoContact.pdf"
-            class="resume-download-link social-icon"
-          >
-            <i
-              class="bi bi-file-earmark-person-fill"
-              title="Resume Download"
-            ></i>
-          </a>
+          <div class="hstack">
+            <p class="subtitle site-subtitle">Software Engineer</p>
+            <div class="social-container">
+              <div class="social-group panel card">
+                <a
+                  role="button"
+                  aria-label="Instagram"
+                  href="https://instagram.com/nathan.laha"
+                  class="social-icon"
+                >
+                  <i class="bi bi-instagram" title="Instagram"></i>
+                </a>
+                <a
+                  role="button"
+                  href="https://github.com/nlaha"
+                  class="social-icon"
+                >
+                  <i class="bi bi-github" title="GitHub"></i>
+                </a>
+                <a
+                  role="button"
+                  aria-label="LinkedIn"
+                  href="https://www.linkedin.com/in/nlaha"
+                  class="social-icon"
+                >
+                  <i class="bi bi-linkedin" title="LinkedIn"></i>
+                </a>
+                <a
+                  role="button"
+                  href="mailto:nlaha@nlaha.com"
+                  class="social-icon"
+                >
+                  <i class="bi bi-envelope-at-fill" title="Email"></i>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
@@ -77,75 +58,44 @@
             {{ generic.summary }}
           </p>
           <div style="display: flex; gap: 1em">
-            <a href='https://ko-fi.com/B0B1CDW9P' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi3.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+            <a
+              aria-label="Blog"
+              role="button"
+              class="panel-button"
+              href="https://blog.nlaha.com/"
+            >
+              Blog
+            </a>
+            <a href="https://ko-fi.com/B0B1CDW9P" target="_blank"
+              ><img
+                height="36"
+                style="border: 0px; height: 36px"
+                src="https://storage.ko-fi.com/cdn/kofi3.png?v=3"
+                alt="Buy Me a Coffee at ko-fi.com"
+            /></a>
           </div>
-          <a aria-label="Blog" role="button" href="https://blog.nlaha.com/">
-            <button class="panel-button">Blog</button>
-          </a>
         </div>
       </div>
     </nav>
+
     <nav class="panel card">
-      <p class="panel-heading">Education & Experience</p>
-      <div
-        class="panel-block"
-        v-for="(experience, index) in experiences"
-        v-bind:class="{ last_panel_block: index === experiences.length - 1 }"
-        v-bind:key="experience.title"
-      >
-        <div class="panel-content">
-          <h3 class="panel-title">{{ experience.title }}</h3>
-          <h4>
-            <i>{{ experience.date }}</i>
-          </h4>
-          <p class="panel-description">{{ experience.description }}</p>
-          <a :href="experience.url"
-            ><button class="panel-button" v-if="experience.url">
-              Learn More
-            </button></a
-          >
-        </div>
-      </div>
+      <p class="panel-heading">Education</p>
+      <Section :items="education" :showDate="true" />
+    </nav>
+
+    <nav class="panel card">
+      <p class="panel-heading">Experience</p>
+      <Section :items="experiences" />
     </nav>
 
     <nav class="panel card">
       <p class="panel-heading n-panel-heading">Programming Projects</p>
-      <div
-        class="panel-block spotlight-group"
-        v-for="(project, index) in programming"
-        v-bind:class="{ last_panel_block: index === programming.length - 1 }"
-        v-bind:key="project.title"
-      >
-        <div class="panel-content">
-          <h3 class="panel-title">{{ project.title }}</h3>
-          <p class="panel-description">{{ project.description }}</p>
-          <a :href="project.url"
-            ><button class="panel-button" v-if="project.url">
-              Learn More
-            </button></a
-          >
-        </div>
-      </div>
+      <Section :items="programming" />
     </nav>
 
     <nav class="panel card">
       <p class="panel-heading n-panel-heading">Art Projects</p>
-      <div
-        class="panel-block"
-        v-for="(project, index) in art"
-        v-bind:class="{ last_panel_block: index === art.length - 1 }"
-        v-bind:key="project.title"
-      >
-        <div class="panel-content">
-          <h3 class="panel-title">{{ project.title }}</h3>
-          <p class="panel-description">{{ project.description }}</p>
-          <a :href="project.url"
-            ><button class="panel-button" v-if="project.url">
-              Learn More
-            </button></a
-          >
-        </div>
-      </div>
+      <Section :items="art" />
     </nav>
   </div>
 </template>
@@ -162,10 +112,13 @@
 
 <script>
 import siteData from "./SiteData";
+import Section from "./Section.vue";
 
 export default {
   name: "App",
-  components: {},
+  components: {
+    Section,
+  },
   data() {
     return siteData;
   },
--- src/SiteData.js
+++ src/SiteData.js
@@ -2,7 +2,7 @@ var siteData = {
   darkMode: false,
   userTheme: "light-theme",
   generic: {
-    summary: `I'm a 4th-year undergraduate student at Washington State University studying Computer Science. This website acts as a resume as well as a place to show off projects and share social media links.`,
+    summary: `I'm a software engineer based in Seattle, WA. This website acts as a resume as well as a place to show off projects and share social media links.`,
   },
   contacts: [
     {
@@ -22,78 +22,114 @@ var siteData = {
       value: "https://github.com/nlaha",
     },
   ],
+  education: [
+    {
+      title: "Washington State University - B.Sc. CS",
+      date: "2021 - May 5th, 2025",
+      url: "https://school.eecs.wsu.edu/academics/undergraduate-program/computer-science/",
+    },
+  ],
   experiences: [
+    {
+      title: "Stealth Startup - Software Engineer",
+      date: "2023 - Present",
+      description: [
+        "Developed 3 full-stack web applications from scratch to meet business needs",
+        "Set up an Azure tenant using Bicep IaC for continuous deployment",
+        "Managed an Azure Kubernetes Service (AKS) cluster",
+        "Enhanced an open-source project with new features",
+        "Provided support for several production applications",
+      ],
+    },
     {
       title: "Premera Blue Cross - Software Engineer Intern",
       date: "Summer 2024",
-      description: "Working as a full-time intern on the medical management team, I contributed to several back-end services to improve the efficiency and traceability of Premera data. During this time I helped identify and fix 15+ bugs in an application during UAT.",
+      description: [
+        "Fixed 15+ bugs in a C# back-end API",
+        "Worked on an agile team",
+        "Wrote test plans and utilized SQL to gather test data",
+        "Used test driven development (TDD) to build applications",
+        "Integrated application insights monitoring into several microservices",
+      ],
       url: "https://www.premera.com",
     },
     {
       title: "Premera Blue Cross - Systems Engineering Intern",
       date: "Summer 2023",
-      description: "Working as a full-time intern in systems engineering at Premera Blue Cross. I helped the cloud platform team implement policy and Terraform code across an Azure tenant. Ultimately, this enabled 5 teams across the company better utilize Azure while staying compliant with HIPAA and HITRUST standards.",
+      description: [
+        "Utilized Azure Policy to enforce HIPAA compliance across an Azure tenant",
+        "Used Bicep and Terraform to interact with Azure",
+        "Worked on an agile team utilizing Azure DevOps",
+      ],
       url: "https://www.premera.com",
     },
     {
       title: "WSU VCEA - Teaching Assistant",
       date: "Fall Semester 2023",
-      description: "Working as a TA for the Voiland college of engineering, I taught a 3-hour lab section once a week, helping 17 students review content and prepare for their exams. I also held office hours where I provided one-on-one assistance to those that needed it."
+      description: [
+        "Taught a 3-hour lab section for 17 students",
+        "Held office hours and provided one-on-one help for students",
+        "Described C programming concepts during lab and office hours",
+      ],
     },
     {
       title: "UW Medicine - Program Assistant",
       date: "2020 - Present",
-      description: "Working as a part-time software engineer helping to create web apps and data visualizations using Python, R, and SQL. During this time, I developed a full-stack web application to display and analyze laboratory test metrics. I also worked on a research project as part of the Foy Lab at UW Medicine.",
+      description: [
+        "Created data visualizations using Python and R",
+        "Developed several production ready full-stack web apps in Python",
+        "Worked with the Foy Lab to develop a machine learning approach for test interpretations",
+      ],
       url: "https://foylab.xyz/people.html",
-    },
-    {
-      title: "Stealth Startup - Freelance Software Engineer",
-      date: "2023 - Present",
-      description: "Working as a freelance software engineer, I developed a full-stack web application using React and TypeScript. This was a re-write of an open-source project I had developed years earlier called Equinox Dialogue that had the attention of the founders for its ease of use.",
     },
     {
       title: "UW Medicine - Volunteer",
       date: "2019 - 2020",
-      description: "Working as a volunteer helping with small data science tasks in R and Python. During this time, I learned the R programming language and developed an application to replace a SQL script that was no longer maintained. By doing this, I unified a codebase that had previously relied on SQL to manage data intake.",
+      description: [
+        "Helped with several data science tasks using Python and R",
+        "Converted a SQL script to R, allowing for easier development",
+      ],
       url: "",
-    },
-    {
-      title: "Washington State University - B.Sc. CS",
-      date: "2021 - 2025",
-      description: "Working towards a Bachelor of Science in computer science from the WSU Voiland College of Engineering and Architecture.",
-      URL: "https://school.eecs.wsu.edu/academics/undergraduate-program/computer-science/",
     },
     {
       title: "Crimson Code Hackathon",
       date: "2022 - 2022",
-      description: 'Participated in the 2022 Crimson Code hackathon with a team of 4. Our web app "Coug Study Network" won first place.',
-      URL: "https://dailyevergreen.com/128124/news/students-compete-learn-new-coding-in-annual-hackathon/",
+      description: [
+        "Won 1st place in our category for this 24-hour hackathon",
+        "Developed a react app with a C# back-end along with 3 other team members",
+      ],
+      url: "https://dailyevergreen.com/128124/news/students-compete-learn-new-coding-in-annual-hackathon/",
     },
     {
-      title: "Ballard High School - APCS",
-      date: "2017 - 2021",
-      description: "My first formal computer science class. Used the JAVA programming language and passed the AP exam with a 5/5.",
-      url: "",
-    },
-    {
-      title: "BHS General Computing Club",
+      title: "BHS General Computing Club (GCC)",
       date: "2019 - 2021",
-      description: "Leader of the BHS General Computing Club (GCC). At GCC we create a space where people of all skill levels can learn about software/game development, data science and graphic art.",
+      description: [
+        "Co-president of the club along with 2 other students",
+        "Developed projects alongside other club members",
+      ],
       url: "https://gcc.nlaha.com",
     },
   ],
   programming: [
     {
       title: "Godot MIDI",
-      description: "Wrote a custom MIDI file parser from scratch both in C# and later in C++ as a plugin for the Godot game engine. This allows developers to incorporate music synchronization into their games or applications easily. It is an open-source project with over sixty stars on GitHub.",
+      description:
+        "Wrote a custom MIDI file parser from scratch both in C# and later in C++ as a plugin for the Godot game engine. This allows developers to incorporate music synchronization into their games or applications easily. It is an open-source project with over sixty stars on GitHub.",
       url: "https://github.com/nlaha/godot-midi",
       photos: [],
     },
     {
       title: "SPS MC",
-      description: "SPS MC is an Office 365 secured Minecraft server for Seattle Public Schools students. It uses the Office365 API to verify if a player is a part of Seattle Public Schools.",
+      description:
+        "SPS MC is an Office 365 secured Minecraft server for Seattle Public Schools students. It uses the Office365 API to verify if a player is a part of Seattle Public Schools.",
       url: "https://github.com/GCC-Minecraft-Team",
       photos: [],
+    },
+    {
+      title: "The Top",
+      description:
+        "Composed an original soundtrack for a game, and provided additional assistance with programming some more advanced features. It is available for purchase on Steam.",
+      url: "https://store.steampowered.com/app/2096560/The_Top/",
     },
     {
       title: "Equinox Dialogue",
@@ -102,7 +138,8 @@ var siteData = {
     },
     {
       title: "Japanese Art Swords",
-      description: "A fully custom website I made using CraftCMS, I wrote a python script to scrape and import data from the old Drupal site.",
+      description:
+        "A fully custom website I made using CraftCMS, I wrote a python script to scrape and import data from the old Drupal site.",
       url: "https://japaneseartswords.com",
       photos: [],
     },
@@ -113,30 +150,19 @@ var siteData = {
       url: "https://github.com/nlaha/SkyMeshGLTF",
       photos: [],
     },
-    {
-      title: "The Top",
-      description:
-        "Composed an original soundtrack for a game, and provided additional assistance with programming some more advanced features. It is available for purchase on Steam.",
-      url: "https://store.steampowered.com/app/2096560/The_Top/",
-    },
-    {
-      title: "Void World",
-      description:
-        "Working with 4 other programmers and level designers, I took on the role of engine programmer and lead artist to create a first-person puzzle game. We spent a year working on this during the height of COVID-19 and while we didn't feel the product was ready to be put on the market, it's a good showcase of my technical and artistic ability. I created it in Unreal Engine and built a modular C++ framework so that puzzle elements could be easily added by our level designers.",
-      url: "",
-    },
   ],
   art: [
     {
-      title: "3D Art",
-      description:
-        "In my free time, I like doing 3D modeling. It's a skill I want to continue to improve even if it's just for fun. Check out my Artstation page at the link below.",
+      title: "3D Art Portfolio",
+      description: "A collection of 3D art I have made over the years",
       url: "https://www.artstation.com/nlaha",
     },
     {
       title: "Digital Filmmaking",
-      description:
-        'I graduated from the digital filmmaking program at BHS with a department scholar award. In this program, I learned how to create high-quality productions and work in a team effectively, even in stressful situations. My animation "Time Tower" won awards at the Northwest High School Film Festival and the National Academy of Television Arts and Sciences - Northwest Student Emmy Awards.',
+      description: [
+        "Completed four years of the Ballard High School digital film-making program",
+        "Won department scholar for my achievements during that time",
+      ],
       url: "https://vimeo.com/559140574",
     },
   ],
